beca032b31ab831076424c859e6232d33b170a55
Michał Walenciak <Kicer86@gmail.com>
2020-06-11 20:52:08 +0200

    value setter and getter

--- grid.hpp
+++ grid.hpp
@@ -9,7 +9,10 @@ class Grid
             : m_rows(rows)
             , m_cols(cols)
         {
+            m_matrix.reserve(rows);
 
+            for(int i = 0; i < rows; i++)            // fill rows
+                m_matrix.push_back(Row(cols, {}));   // with Row filled with 'cols` items
         }
 
         int rows() const
@@ -22,7 +25,21 @@ class Grid
             return m_cols;
         }
 
+        void set(int r, int c, const T& v)
+        {
+            m_matrix[r][c] = v;
+        }
+
+        const T& get(int r, int c) const
+        {
+            return m_matrix[r][c];
+        }
+
     private:
+        typedef std::vector<T> Row;
+        typedef std::vector<Row> Rows;
+
+        Rows m_matrix;
         const int m_rows;
         const int m_cols;
 };
--- grid_tests.cpp
+++ grid_tests.cpp
@@ -16,4 +16,22 @@ TEST(GridTest, dimensions)
 
     EXPECT_EQ(g1.rows(), 10);
     EXPECT_EQ(g1.columns(), 20);
+
+    Grid<float> g2(3, 6);
+
+    EXPECT_EQ(g2.rows(), 3);
+    EXPECT_EQ(g2.columns(), 6);
+}
+
+
+TEST(GridTest, values)
+{
+    Grid<int> grid(3, 3);
+
+    grid.set(0, 0, 5);
+    grid.set(2, 1, 10);
+
+    EXPECT_EQ(grid.get(0, 0), 5);
+    EXPECT_EQ(grid.get(2, 1), 10);
+    EXPECT_EQ(grid.get(2, 2), 0);
 }
