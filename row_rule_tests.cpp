ea4c1ecf1b2293d05c611bef99dbee2d1f9b2659
Michał Walenciak <Kicer86@gmail.com>
2020-06-12 18:23:54 +0200

    full row test

--- row_rule_tests.cpp
+++ row_rule_tests.cpp
@@ -29,3 +29,20 @@ TEST(RowRuleTest, mixedRow)
     const auto valid_numbers = rule.validNumbers(0, 0);
     EXPECT_EQ(valid_numbers, (std::vector{2, 3, 4}));
 }
+
+
+
+TEST(RowRuleTest, fullRow)
+{
+    const Grid<int> grid
+    {
+        {6, 0, 1, 5, 0, 0},
+        {0, 2, 0, 3, 1, 0},
+        {1, 2, 6, 5, 4, 3}
+    };
+
+    const RowRule rule(grid);
+    const auto valid_numbers = rule.validNumbers(2, 4);
+
+    EXPECT_TRUE(valid_numbers.empty());
+}
