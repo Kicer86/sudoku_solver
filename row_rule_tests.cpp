304104f1bed2438c04bd07a0a24c03cc43d10521
Michał Walenciak <Kicer86@gmail.com>
2020-06-12 18:22:15 +0200

    test row with mix of values and holes

--- row_rule_tests.cpp
+++ row_rule_tests.cpp
@@ -13,3 +13,19 @@ TEST(RowRuleTest, emptyRow)
     const auto valid_numbers = rule.validNumbers(0, 0);
     EXPECT_EQ(valid_numbers, (std::vector{1, 2, 3, 4, 5, 6, 7, 8, 9}));
 }
+
+
+
+TEST(RowRuleTest, mixedRow)
+{
+    const Grid<int> grid
+    {
+        {6, 0, 1, 5, 0, 0},
+        {0, 2, 0, 3, 1, 0}
+    };
+
+    RowRule rule(grid);
+
+    const auto valid_numbers = rule.validNumbers(0, 0);
+    EXPECT_EQ(valid_numbers, (std::vector{2, 3, 4}));
+}
