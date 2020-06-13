db2c0b901462bcfa7afea0425cc725c56baa4eb1
Michał Walenciak <Kicer86@gmail.com>
2020-06-13 07:06:20 +0200

    adding hard test

--- solver_tests.cpp
+++ solver_tests.cpp
@@ -50,3 +50,49 @@ TEST(SolverTest, easyPuzzle1)
     EXPECT_THAT(steps, Contains( std::make_tuple(8, 6, 4)) );
     EXPECT_THAT(steps, Contains( std::make_tuple(8, 8, 1)) );
 }
+
+
+/*
+TEST(SolverTest, hardPuzzle1)
+{
+    const Grid<int> grid
+    {
+        { 1, 0, 0,  0, 7, 0,  0, 3, 0 },
+        { 8, 3, 0,  6, 0, 0,  0, 0, 0 },
+        { 0, 0, 2,  9, 0, 0,  6, 0, 8 },
+
+        { 6, 0, 0,  0, 0, 4,  9, 0, 7 },
+        { 0, 9, 0,  0, 0, 0,  0, 5, 0 },
+        { 3, 0, 7,  5, 0, 0,  0, 0, 4 },
+
+        { 2, 0, 3,  0, 0, 9,  1, 0, 0 },
+        { 0, 0, 0,  0, 0, 2,  0, 4, 3 },
+        { 0, 4, 0,  0, 8, 0,  0, 0, 9 }
+    };
+
+    Solver solver(grid);
+
+    const auto steps = solver.generateSteps();
+
+    ASSERT_EQ(steps.size(), 18);
+    EXPECT_THAT(steps, Contains( std::make_tuple(0, 0, 1)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(0, 2, 3)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(1, 3, 1)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(1, 5, 9)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(2, 6, 1)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(2, 8, 5)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(3, 0, 8)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(3, 1, 1)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(4, 3, 8)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(4, 5, 1)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(5, 7, 1)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(5, 8, 2)) );
+
+    EXPECT_THAT(steps, Contains( std::make_tuple(6, 0, 5)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(6, 2, 2)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(7, 3, 9)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(7, 5, 3)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(8, 6, 4)) );
+    EXPECT_THAT(steps, Contains( std::make_tuple(8, 8, 1)) );
+}
+*/
